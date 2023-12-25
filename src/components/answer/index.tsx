184ba6561baf610d30abe8a373ae159ec6817691
Sheng Hong Tian <stefan55831@gmail.com>
2023-12-25 19:35:11 +0800

    Finish ask and feel lucky button logic

--- src/components/answer/index.tsx
+++ src/components/answer/index.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState } from 'react';
+import Button from '../button';
+import styles from './answer.module.scss';
+
 interface Props {
   answer: string;
+  onAskAnother: () => void;
 }
 
-const Answer = ({ answer }: Props) => {
+const DELAY = 30;
+
+const Answer = ({ answer, onAskAnother }: Props) => {
+  const [typedAnswer, setTypedAnswer] = useState('');
+  const [isTypingCompleted, setIsTypingCompleted] = useState(false);
+
+  useEffect(() => {
+    setTypedAnswer('');
+
+    const interval = setInterval(() => {
+      setTypedAnswer((currentTyped) => {
+        if (currentTyped.length < answer.length) {
+          return currentTyped + answer[currentTyped.length];
+        } else {
+          clearInterval(interval);
+          setIsTypingCompleted(true);
+          return currentTyped;
+        }
+      });
+    }, DELAY);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [answer]);
+
   return (
-    <div className="answer">
-      <p>{answer}</p>
+    <div className={styles.container}>
+      <p className={styles.answer}>
+        <span className={styles.bold}>Answer:</span> {typedAnswer}
+      </p>
+      {isTypingCompleted && (
+        <Button onClick={onAskAnother}>Ask another question</Button>
+      )}
     </div>
   );
 };
---
+++ src/components/question-form/constants.tsx
@@ -0,0 +1,11 @@
+export const questions = [
+  'What is the meaning of life?',
+  'What is happiness?',
+  'How can I be happy?',
+  'How can I be happier?',
+  'What is the best way to treat others?',
+  'What is the best way to treat myself?',
+  'What is the best way to live?',
+  'How to live a good life?',
+  'What is a healthy relationship?',
+];
